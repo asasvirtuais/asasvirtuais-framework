f3b76812b72cff005f70a0cc814506455ebb541e
Icaro Asas Virtuais <asasvirtuais.dev@gmail.com>
2026-02-17 06:44:16 -0300

    bump version to 2.3.2 and refactor useActionProvider for improved error handling and callback management

--- packages/action.tsx
+++ packages/action.tsx
@@ -5,7 +5,7 @@ export type ActionProps<Params, Result> = {
   params: Partial<Params>
   action: (fields: Params) => Promise<Result>
   onResult?: (result: Result) => any
-  onError?: (error: Error) => any 
+  onError?: (error: Error) => any
   autoTrigger?: boolean
 }
 
@@ -13,11 +13,6 @@ export function useActionProvider<Fields, Result>(props: React.PropsWithChildren
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState<Error | null>(null)
   const [result, setResult] = useState<Result | null>(null)
-
-  useEffect(() => {
-    if (props.autoTrigger)
-      props.action(props.params as Fields)
-  }, [])
 
   /** receives an event (optional) and processes the form submission using the fields state as props, stores the result or error in the respective states */
   const submit = useCallback(
@@ -44,20 +39,46 @@ export function useActionProvider<Fields, Result>(props: React.PropsWithChildren
     [props.params, props.action, props.onError]
   )
 
+  const callback = useCallback(async (fields: Fields) => {
+    setLoading(true)
+    setError(null)
+    try {
+      const result = await props.action(fields)
+      if (props.onResult)
+        props.onResult(result)
+      setResult(result)
+    } catch (error) {
+      console.error(error)
+      if (props.onError)
+        props.onError(error as Error)
+      setError(error as Error)
+      throw error
+    } finally {
+      setLoading(false)
+    }
+    return false
+  }, [props.action])
+
+  useEffect(() => {
+    if (props.autoTrigger)
+      callback(props.params as Fields)
+  }, [])
+
+
   return {
     loading,
     result,
     error,
     submit,
-    callback: props.action,
+    callback,
     params: props.params,
   }
 }
 
 export const ActionContext = createContext<ReturnType<typeof useActionProvider<any, any>> | undefined>(undefined)
 
-export function ActionProvider<Fields, Result>({children, ...params}: ActionProps<Fields, Result> & {
-  children: React.ReactNode | ( (props: ReturnType<typeof useActionProvider<Fields, Result>>) => React.ReactNode )
+export function ActionProvider<Fields, Result>({ children, ...params }: ActionProps<Fields, Result> & {
+  children: React.ReactNode | ((props: ReturnType<typeof useActionProvider<Fields, Result>>) => React.ReactNode)
 }) {
   const context = useActionProvider<Fields, Result>(params)
   return (
